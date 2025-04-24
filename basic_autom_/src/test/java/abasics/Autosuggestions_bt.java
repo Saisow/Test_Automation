ef4875f319c39e3012a4eb8b7db5c007192c8749
Saisowmya <saisowmyamiryala@gmail.com>
2025-04-24 15:46:47 +0530

    push1

--- basic_autom_/src/test/java/abasics/Autosuggestions_bt.java
+++ basic_autom_/src/test/java/abasics/Autosuggestions_bt.java
@@ -19,6 +19,8 @@ public class Autosuggestions_bt {
 		
 		System.out.println("I'm sai sowmya");
 		
+		System.out.println("Hello");
+		
 		driver.get("https://www.makemytrip.com");    //open the url
 		
 		driver.manage().window().maximize();      //maximize the window
